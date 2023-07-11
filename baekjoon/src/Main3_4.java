50f97b0ee559b48591ff16eef962d7cac9328761
k2ring <anatkh02@gmail.com>
2023-07-12 01:34:35 +0900

    no message

---
+++ baekjoon/src/Main3_4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int all = sc.nextInt();
+		int n = sc.nextInt();
+		int A, B;
+		int sum = 0;
+		int tot = 0;
+
+		for (int i = 1; i <= n; i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			sum = A * B;
+			tot += sum;
+		}
+
+		if (all == tot) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	}
+}
